35067a136acb5d416ac9d2eb8784510ef51b4dea
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-05-30 18:14:04 +0530

    Add files via upload

--- Turbo.js
+++ Turbo.js
@@ -1907,18 +1907,17 @@ message = await prepareWAMessageMedia({ image : { url: search.videos[0].thumbnai
                 Turbo.sendMessage(m.chat, { audio: cnvrt, mimetype: 'audio/mpeg'}, { quoted: msg })
             }
             break
-	        case 'instagram': case 'ig': case 'igdl': case 'insta': {
-                if (!text) throw 'Enter Query Url!'
-                replay(mess.wait)
-                if (/(?:\/p\/|\/reel\/|\/tv\/)([^\s&]+)/.test(isUrl(text)[0])) {
-                    let anu = await fetchJson(api('zenz', '/downloader/instagram2', { url: isUrl(text)[0] }, 'apikey'))
-                    for (let media of anu.data) Turbo.sendMedia(m.chat, media, '', `Download Url Instagram From ${isUrl(text)[0]}`, m)
-                } else if (/\/stories\/([^\s&]+)/.test(isUrl(text)[0])) {
-                    let anu = await fetchJson(api('zenz', '/downloader/instastory', { url: isUrl(text)[0] }, 'apikey'))
-                    Turbo.sendMedia(m.chat, anu.media[0].url, '', `Download Url Instagram From ${isUrl(text)[0]}`, m)
-                }
-            }
-            break
+	        case 'ig':
+case 'igdl':
+case 'instagram':
+if (!text) return reply('The link?')
+var { igDownloader } = require('./lib/igdown')
+   res = await igDownloader(`${c}`).catch(e => {
+reply(mess.error.api)
+})
+console.log(res)
+sendMediaURL(from,`${res.result.link}`,`${res.result.desc}`)
+                    break
             case 'joox': case 'jooxdl': {
                 if (!text) throw 'No Query Title'
                 replay(mess.wait)
