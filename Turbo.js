257f3a4f509e2d76e84cca0d39c39733c2cdb17d
slayer-md <107115437+slayer-md@users.noreply.github.com>
2022-12-22 13:41:56 +0530

    Add files via upload

--- Turbo.js
+++ Turbo.js
@@ -483,7 +483,7 @@ try {
 
 //[push msg to console & autoread]\\
         if (m.message) {
-            Turbo.sendReadReceipt(m.chat, m.sender, [m.key.id])
+            Turbo.sendMessage(m.chat, m.sender, [m.key.id])
             console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
 
